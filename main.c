d4d4d8b4f36c3e7f6009c22b244a31193955dae7
MMeadow <151444099+MMeadow9@users.noreply.github.com>
2024-12-07 17:18:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -10,14 +10,24 @@ typedef struct Time {
 
 void print_time(Time t) {
     // Функция печатающая время
-    printf("%02d:%02d:%02d - format %d\n", (t.hours % t.format), t.minutes, t.seconds, t.format);
+    if (t.format == 12) {
+        if (t.hours > 11) {
+            printf("%02d:%02d:%02d - AM\n", (t.hours % 12), t.minutes, t.seconds);
+        }
+        if (t.hours < 12) {
+            printf("%02d:%02d:%02d - PM\n", (t.hours % 12), t.minutes, t.seconds);
+        }
+    }
+    if (t.format == 24) {
+        printf("%02d:%02d:%02d\n", (t.hours % 24), t.minutes, t.seconds);
+    }
 }
 
 Time add(Time t1, Time t2) {
     // Функция объединяющая два времени
     int seconds = t1.seconds + t2.seconds;
     int minutes = t1.minutes + t2.minutes + seconds / 60;
-    int hours = (t1.hours + t2.hours + (minutes / 60)) % t1.format;
+    int hours = (t1.hours + t2.hours + (minutes / 60)) % 24;
     
     seconds %= 60;
     minutes %= 60;
@@ -31,7 +41,7 @@ Time sub(Time t1, Time t2) {
 
     int seconds = all_seconds % 60;
     int minutes = all_seconds / 60 % 60;
-    int hours = (all_seconds / 3600) % t1.format;
+    int hours = (all_seconds / 3600) % 24;
 
     return (Time){t1.format, hours, minutes, seconds};
 }
@@ -39,6 +49,11 @@ Time sub(Time t1, Time t2) {
 int main() {
     Time t1 = {24, 18, 40, 20};
     Time t2 = {24, 11, 55, 35};
-    Time t3 = sub(t1, t2);  // Time{24, 6, 44, 45}
-    print_time(t3);  // "06:44:45 - format 24"
+    Time t3 = {12, 18, 40, 20};
+    Time t4 = {12, 11, 55, 35};
+    
+    print_time(t1);  // 18:40:20
+    print_time(t2);  // 11:55:35
+    print_time(t3);  // 06:40:20 - AM
+    print_time(t4);  // 11:55:35 - PM
 }
